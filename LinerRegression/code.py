32dbac2841437885f450d387779a4505bcd93b63
kkzzzzzz <zhukaidj@gmail.com>
2017-10-03 11:45:14 +0800

    LR

---
+++ LinerRegression/code.py
@@ -0,0 +1,34 @@
+#coding:utf-8 
+#通过组合最小二乘法和梯度下降法，可以得到线性回归
+
+wheat_and_bread =  [[1, 6], [2, 5], [3, 7], [4, 10]]
+
+
+def step_gradient(b_current, m_current, points, learingRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0,len(points)):
+        x  = points[i][0]
+        y  = points[i][1]
+        # 损失函数，对b求导
+        b_gradient += -2 * (y - ((m_current * x + b_current)))
+        # 损失函数，对m求导
+        m_gradient += -2 * x * (y - (m_current * x) + b_current)
+    b_gradient = b_gradient / N
+    m_gradient = m_gradient / N
+    new_b = b_gradient - (learingRate * b_gradient)
+    new_m = m_gradient - (learingRate * m_gradient)
+    return [new_b, new_m]
+
+
+def gradient_descent_runner(points, start_b, start_m, learing_rate, num_iterations):
+    b = start_b
+    m = start_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, points, learing_rate)
+    return [b, m]
+
+
+s = gradient_descent_runner(wheat_and_bread, 1, 1, 0.01, 100)
+print s
